afc1cf15c8b127005738bdf39e314fb2eb98034f
karneus-develop <karneus-develop@yandex.ru>
2022-08-06 17:32:55 +0300

    Test

---
+++ Connetcion_Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VapeWorkBench
+{
+    internal class Connetcion_Test
+    {
+    }
+}
